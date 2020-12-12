9e670e3104a8387e7e7787adefaef26de31e90e5
ivonaandova <74076663+ivonaandova@users.noreply.github.com>
2020-12-12 20:25:09 +0100

    CoffeeBar changes

--- MVC APP/MVC APP/Models/CoffeeBar.cs
+++ MVC APP/MVC APP/Models/CoffeeBar.cs
@@ -2,16 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Razor.Text;
 
 namespace MVC_APP.Models
 {
-    public class CaffeBar
+    public class CoffeeBar
     {
         public string Name { get; set; }
         public float Distance { get; set; }
         public string WebPage { get; set; }
-        public string Location {get; set;}
-
+        public string Location { get; set; }
+        public float Rating { get; set; }
     }
 }
