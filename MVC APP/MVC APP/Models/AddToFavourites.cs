048b398987430b81dcd31ba4de1f55d2c099c0ce
MartinVrangalovski <74061702+MartinVrangalovski@users.noreply.github.com>
2020-12-12 20:11:04 +0100

    models

---
+++ MVC APP/MVC APP/Models/AddToFavourites.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_APP.Models
+{
+    public class AddToFavourites
+    {
+        public class AddToStore
+        {
+            public List<CaffeBar> bars { get; set; }
+            public int selectedCaffeBar { get; set; }
+            public AddToStore()
+
+            {
+                bars = new List<CaffeBar>();
+            }
+        }
+    }
+}
---
+++ MVC APP/MVC APP/Models/CaffeBar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Razor.Text;
+
+namespace MVC_APP.Models
+{
+    public class CaffeBar
+    {
+        public string Name { get; set; }
+        public float Distance { get; set; }
+        public string WebPage { get; set; }
+        public string Location {get; set;}
+
+    }
+}
