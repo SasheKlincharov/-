a3f20e22816b2d97f54e9a1d508876f3845d8fd1
damchevski <42002981+damchevski@users.noreply.github.com>
2021-01-27 16:27:13 +0100

    api services domasna4

---
+++ MVC APP/MVC APP/Controllers/CoffeeBarsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MVC_APP.Models;
+
+namespace MVC_APP.Controllers
+{
+    public class CoffeeBarsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/CoffeeBars
+        public IQueryable<CoffeeBar> GetBars()
+        {
+            return db.Bars;
+        }
+
+        // GET: api/CoffeeBars/5
+        [ResponseType(typeof(CoffeeBar))]
+        public IHttpActionResult GetCoffeeBar(int id)
+        {
+            CoffeeBar coffeeBar = db.Bars.Find(id);
+            if (coffeeBar == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(coffeeBar);
+        }
+
+        // PUT: api/CoffeeBars/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCoffeeBar(int id, CoffeeBar coffeeBar)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != coffeeBar.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(coffeeBar).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CoffeeBarExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CoffeeBars
+        [ResponseType(typeof(CoffeeBar))]
+        public IHttpActionResult PostCoffeeBar(CoffeeBar coffeeBar)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Bars.Add(coffeeBar);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = coffeeBar.Id }, coffeeBar);
+        }
+
+        // DELETE: api/CoffeeBars/5
+        [ResponseType(typeof(CoffeeBar))]
+        public IHttpActionResult DeleteCoffeeBar(int id)
+        {
+            CoffeeBar coffeeBar = db.Bars.Find(id);
+            if (coffeeBar == null)
+            {
+                return NotFound();
+            }
+
+            db.Bars.Remove(coffeeBar);
+            db.SaveChanges();
+
+            return Ok(coffeeBar);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CoffeeBarExists(int id)
+        {
+            return db.Bars.Count(e => e.Id == id) > 0;
+        }
+    }
+}
--- MVC APP/MVC APP/Global.asax.cs
+++ MVC APP/MVC APP/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace MVC_APP
 {
@@ -12,6 +13,7 @@ namespace MVC_APP
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
